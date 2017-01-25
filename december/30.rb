9276407030adf6abce782b6e5d47cd1c4ac8ecca
mikesuprise1 <mikesupris1@gmail.com>
2017-01-25 10:45:07 -0700

    Learning exercises

--- december/30.rb
+++ december/30.rb
@@ -1,5 +1,11 @@
 require 'rspec'
 
+class Hash
+	def param_converter
+		self.map { |i| i * "=" } * "&"
+	end
+end
+
 describe 'HTML Param Converter' do
   it 'Adds an HTML param converter to the Hash class' do
     hash = { :topic => "baseball", :team => "astros" }
