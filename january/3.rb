06107073e9c5e858f3dd92003bc5e9ae18b0ef11
mikesuprise1 <mikesupris1@gmail.com>
2017-01-27 10:58:39 -0700

    Learing more ruby

--- january/3.rb
+++ january/3.rb
@@ -2,9 +2,13 @@ require 'rspec'
 
 class String
   def total_words
+    scan(/\w+/).count
   end
 
   def word_list
+    list = Hash.new(0)
+    downcase.scan(/\w+/) { |w| list[w] +=1}
+    list
   end
 end
 
