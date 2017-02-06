cdc027c8bd691627da6c1ff7aee1b59df3c3be98
mikesuprise1 <mikesupris1@gmail.com>
2017-02-06 10:51:51 -0700

    Learning more ruby

--- january/13.rb
+++ january/13.rb
@@ -1,6 +1,21 @@
 require 'rspec'
 
 def hashed_fizz_buzz num
+	hash = Hash.new
+
+	1.upto(num).each do |e|
+		if (e % 3 == 0) && (e % 5 == 0)
+			hash[e] = "FizzBuzz"
+		elsif e % 3 == 0
+			hash[e] = "Fizz"
+		elsif e % 5 == 0
+			hash[e] = "Buzz"
+		else
+			hash[e] = e
+		end
+	end
+				
+	hash
 end
 
 describe 'Hashed FizzBuzz' do
